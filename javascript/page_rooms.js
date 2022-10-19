9e4ce7ab2882e0b8e5bf1af00e28cdbe51d3bbe9
Lê Minh Đại <20521153@gm.uit.edu.vn>
2022-10-19 22:35:07 +0700

    19/10- Đại

--- javascript/page_rooms.js
+++ javascript/page_rooms.js
@@ -1,6 +1,30 @@
-const btnChoice=document.querySelector('.filter-block');
+const btnPrice = document.querySelector('.price-js');
+const btnColor = document.querySelector('.color-js');
+const btnType = document.querySelector('.type-js');
 
-btnChoice.addEventListener('click',function(){
-    document.querySelector('.filter-block')
+btnPrice.addEventListener('click',function(){
+    document.querySelector('.price-choice').classList.add('open-filter');
 })
+
+btnColor.addEventListener('click',function(){
+    document.querySelector('.color-choice').classList.add('open-filter');
+})
+
+btnType.addEventListener('click',function(){
+    document.querySelector('.type-choice').classList.add('open-filter');
+})
+
+
+function closeFilterprice(){
+    document.querySelector('.price-choice').classList.remove('open-filter');
+}
+
+
+
+
+
+
+
+
+
 
