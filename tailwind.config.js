8f3b8417c7a5466033dfaf40111093c91a62cd21
Đông Phương <20520282@gm.uit.edu.vn>
2022-10-26 14:45:54 +0700

    Revert "Merge branch 'create-header-and-footer' of https://github.com/leminhdai2088/alone_dolphin"
    This reverts commit 5f503ad9fc9a2f503cf2c981edcf03d706dee2ca, reversing
    changes made to 01425ec7252637d61685805612b0279507e1492e.

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  mode: "jit",
-  content: ["./src/**/*.{html,js}", "./layout/**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
