3e4cdab23a0c9f07040e961e0fb341097fc8dd53
Nguyễn Tuấn Kha <20521427@gm.uit.edu.vn>
2022-11-01 22:02:24 +0700

    update

--- javascript/page_home.js
+++ javascript/page_home.js
@@ -38,3 +38,28 @@ $(document).ready(function(){
         ]
     });
   });
+  $(document).ready(function(){
+    $('.image-slider-0').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true, //false: khong vo tan
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        draggable: true, //keo tha
+        prevArrow:`<button type='button' class='slick-prev slick-arrow'><ion-icon name="chevron-back-outline"></ion-icon></button>`,
+        nextArrow:`<button type='button' class='slick-next slick-arrow'><ion-icon name="chevron-forward-outline"></ion-icon></button>`,
+        responsive:[
+            {
+                breakpoint: 1025,
+                settings: {
+                slidesToShow: 2,
+                    }
+            },
+        ]
+    });
+  });
+  
+  
+  $(window).load(function() {
+    $('#slider').nivoSlider();		
+});
