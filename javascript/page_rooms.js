67dd5aa434485336da90d16c2df0ff6b9b7e368f
Lê Minh Đại <20521153@gm.uit.edu.vn>
2022-11-19 11:17:19 +0700

    updateASdasdasdasdasdasdasdfgfhgfsghbfgnzdf

--- javascript/page_rooms.js
+++ javascript/page_rooms.js
@@ -71,7 +71,46 @@ document.addEventListener(
 
 
 
-
+// slider
+$(document).ready(function(){
+  $('.image-slider-2').slick({
+      dots: false,
+      arrows: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      infinite: true, //false: khong vo tan
+      // autoplay: true,
+      // autoplaySpeed: 2000,
+      draggable: true, //keo tha
+      prevArrow:  $(".owl-prev"),
+      nextArrow: $(".owl-next"),
+      responsive:[
+          {
+              breakpoint: 1200,
+              settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '30px',
+                  lidesToShow: 4,    
+              },
+              breakpoint: 600,
+              settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '30px',
+                  lidesToShow: 3,    
+              },
+              breakpoint: 0,
+              settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '30px',
+                  lidesToShow: 2,    
+              },
+          },
+      ]
+  });
+});
 
 
 
