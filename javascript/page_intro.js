47eecc8420f7ef6a96b6e9497c3cf60b31df0460
vongchanhkhin <20521460@gm.uit.edu.vn>
2022-11-20 23:07:19 +0700

    update

--- javascript/page_intro.js
+++ javascript/page_intro.js
@@ -1,40 +1,41 @@
 
-$(document).ready(function(){
-    $('.image-slider-2').slick({
-        dots: false,
-        arrows: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        infinite: true, //false: khong vo tan
-        // autoplay: true,
-        // autoplaySpeed: 2000,
-        draggable: true, //keo tha
-        prevArrow:  $(".owl-prev"),
-        nextArrow: $(".owl-next"),
-        responsive:[
-            {
-                breakpoint: 1200,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '30px',
-                    lidesToShow: 4,    
-                },
-                breakpoint: 600,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '30px',
-                    lidesToShow: 3,    
-                },
-                breakpoint: 0,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '30px',
-                    lidesToShow: 2,    
-                },
-            },
-        ]
-    });
-  });
+// $(document).ready(function(){
+    
+// });
+
+$('.img-container').slick({
+    dots: false,
+    arrows: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    infinite: true, //false: khong vo tan
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    draggable: true, //keo tha
+    prevArrow:  $(".owl-prev"),
+    nextArrow: $(".owl-next"),
+    responsive:[
+        {
+            breakpoint: 1024,
+            settings: {
+                
+                slidesToShow: 3,
+                slidesToScroll: 3  
+            }            
+        },
+        {
+            breakpoint: 740,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2 
+            } 
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1 
+            } 
+        }
+    ]
+});
