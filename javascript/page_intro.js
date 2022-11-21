0085273e7e15ec98b32725cc36d8c278f0b84d7f
vongchanhkhin <20521460@gm.uit.edu.vn>
2022-11-21 11:39:48 +0700

    update

--- javascript/page_intro.js
+++ javascript/page_intro.js
@@ -1,41 +1,37 @@
 
-// $(document).ready(function(){
-    
-// });
-
-$('.img-container').slick({
-    dots: false,
-    arrows: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    infinite: true, //false: khong vo tan
-    // autoplay: true,
-    // autoplaySpeed: 2000,
-    draggable: true, //keo tha
-    prevArrow:  $(".owl-prev"),
-    nextArrow: $(".owl-next"),
-    responsive:[
-        {
-            breakpoint: 1024,
-            settings: {
-                
-                slidesToShow: 3,
-                slidesToScroll: 3  
-            }            
-        },
-        {
-            breakpoint: 740,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2 
-            } 
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1 
-            } 
-        }
-    ]
+$(document).ready(function(){
+    $('.img-container').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true, //false: khong vo tan
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        
+        draggable: true, //keo tha
+        prevArrow:  `<div class="owl-prev">
+                        <i class="fa fa-angle-left"></i>
+                    </div>`,
+        nextArrow: `<div class="owl-next">
+                        <i class="fa fa-angle-right"></i>
+                    </div>`,
+        responsive:[
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3  
+                }            
+            },
+            {
+                breakpoint: 740,
+                settings: {
+                   
+                    slidesToShow: 2,
+                    slidesToScroll: 2 
+                } 
+            }
+        ]
+    });
 });
+
+
