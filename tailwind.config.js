c36f11cdb5e4ae497e05d3043deae7ed77fd69d8
Đông Phương <20520282@gm.uit.edu.vn>
2022-11-28 16:11:35 +0700

    edit breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*.{html,js}", "./layout/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        md: "740px",
+        // => @media (min-width: 740px) { ... }
+      },
+    },
+    screens: {
+      tab: "740px",
+      // => @media (min-width: 740px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 };
